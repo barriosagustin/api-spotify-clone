cede8929478d2ecd48161e5056b66028a70964d0
abarrios <agusab2000@gmail.com>
2025-01-15 17:58:33 +0100

    update public variables

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use("/api/1.0", require("./app/routes"));
 
+const URL_PUBLIC = process.env.URL_PUBLIC || `http://localhost:${PORT}`;
+
 app.listen(PORT, () => {
-  console.log(`Tu API es http://localhost:${PORT}/api/1.0`);
+  console.log(`Tu API está disponible en ${URL_PUBLIC}/api/1.0`);
+});
+
+app.get("/test", (req, res) => {
+  res.send("backend works");
 });
